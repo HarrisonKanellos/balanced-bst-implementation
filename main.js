84c5d1615873455d1dd400b8b1a19ff03b124d81
HarrisonKanellos <harrisonkanellos@gmail.com>
2026-06-10 00:34:17 +1000

    Create main.js with driver code to test balanced BST implementation

---
+++ main.js
@@ -0,0 +1,14 @@
+import { createTree } from "./binarySearchTree.js";
+
+const randArrayOfLen = (len) => {
+    const array = [];
+    for (let i = 0; i < len; i++) {
+        array.push(Math.floor(Math.random() * 100));
+    }
+    return array;
+}
+
+const randArray = randArrayOfLen(12);
+console.log(randArray);
+
+const bst = createTree(randArray);
